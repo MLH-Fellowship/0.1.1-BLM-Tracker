d1193c583850666bba5ddcdc82a3cefef4ee9881
Parthiv Chigurupati <parthivc0930@gmail.com>
2020-06-02 20:36:12 -0400

    Created API Key file template

---
+++ twitterAPIKeys.py
@@ -0,0 +1,10 @@
+#
+# Created by Parthiv Chigurupati, Stella Wang, and Amir Yalamov
+#
+
+
+# Insert your Twitter API keys here!
+apiKey = ""
+apiSecretKey = ""
+accessToken = ""
+accessTokenSecret = ""
