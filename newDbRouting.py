7f851fc7038226bb62d2f687e10b6bc954cb34e6
stellaw1 <stella01wang@gmail.com>
2020-06-04 15:42:53 -0700

    flask connected to html. format needs iteration

--- newDbRouting.py
+++ newDbRouting.py
@@ -17,55 +17,26 @@ def fetch_tweets():
   db = client.tweetDatabase
   coll = db.tweetCollection
 
-  #cursor = coll.find({},tailable=True,timeout=False)
-
-  # iterate through db and send to front end
-
-  # ------------------------------ this section is just for testing if everything is running, feel free to comment all this out ------------------------------
-
-  # test to see if connection properly made
-  print ("\nReturn every document:")
-
-  #  ---------- iterate and store all IDs in collection
-  ids = [] # create empty list for IDs
-  for doc in coll.find():
-    ids += [doc["_id"]]
-
-  # print out all IDs
-  print ("IDs: ", ids)
-  print ("total docs: ", len(ids))
-
-  # ---------- get all documents in a collection and put then in a list
-  # returns a list of mondodb documents in the form of Python dictionaries
-  documents = list(coll.find())
-
-  # test if documents have been retrieved properly
-  for doc in documents:
-      # access each document's "_id" key
-      print ("\ndoc _id:", doc["_id"])
-
-  # ------------------------------ end of testing section ------------------------------
-
-
-  # ******************* beginning of iterator section *******************
-
-  # section of code that goes through tweets and adds lat and long of tweets into 'data' so 'data' can later be passed into the html file
-  docs.coll.find()
+  # goes through tweets and adds lat and long of tweets into 'data' so 'data' can later be passed into the html file
+  docs = coll.find()
   data = [] # empty array to store lat and long values as for each tweet in dictioary format. Will be an array of dictionaries
 
   # next() is supposed to return something like {'_id': ObjectId('5cda8b3b665444800ad30129'), 'field': 'value'}, which should be the document. As such, feel free to change the variable names below to reflect how our documents look
 
   # iterate through db collection and add each document's latitude and longitude
-  for lat, long in docs.next().items():
-      data += {'lat': lat, 'long': long}
+  for item in docs:
+    # print(item['place']['bounding_box']['coordinates'][0][0][0])
+    i = {'lat': item['place']['bounding_box']['coordinates'][0][0][0], 'long': item['place']['bounding_box']['coordinates'][0][0][1]}
+    data.append(i)
+  
+  print(data)
+  
+  return render_template('test.html', data=data)
 
-  # ******************* end of iterator section *******************
-
-
-  # example of data to pass to js file
-  #data = {'username': 'tweet', 'site': 'stackoverflow.com'}  # I commented this out since the data from a few lines above should be able to be passed in as a parameter. If not then feel free to make adjustments and let me know
-  return render_template('newHeatmap.html', data=data)
-
+# @app.before_first_request(fetch_tweets)
+# @app.route("/")
+# def main():
+#   return render_template('/static/test.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1')
