cc7cf7974bb1b39ce49181422797d87e3fb1c7b8
stellaw1 <stella01wang@gmail.com>
2020-06-03 19:22:09 -0700

    basic db setup and store/ find functions

--- db.py
+++ db.py
@@ -1,39 +1,24 @@
 from pymongo import MongoClient
 import json
 from random import randint
+import datetime
 
 # Connect to MongoDB
 client = MongoClient('mongodb://localhost:27017')
 db = client.tweetDatabase
-test = db.testCollection
 col = db.tweetCollection
+#db.command('convertToCapped', 'tweetCollection', size=5242880)
 print("connected to MongoDB:", client["HOST"])
 
 
 # function that adds one json obj to database collection
 def store_tweet(tw):
-    test.insert_one(tw)
+    col.insert_one(tw)
 
 
-# sample data for testing connection to db
-names = ['Kitchen', 'Animal', 'State', 'Tastey', 'Big', 'City',
-         'Fish', 'Pizza', 'Goat', 'Salty', 'Sandwich', 'Lazy', 'Fun']
-company_type = ['LLC', 'Inc', 'Company', 'Corporation']
-company_cuisine = ['Pizza', 'Bar Food', 'Fast Food',
-                   'Italian', 'Mexican', 'American', 'Sushi Bar', 'Vegetarian']
-for x in range(1, 51):
-    business = {
-        'name': names[randint(0, (len(names)-1))] + ' ' + names[randint(0, (len(names)-1))] + ' ' + company_type[randint(0, (len(company_type)-1))],
-        'rating': randint(1, 5),
-        'cuisine': company_cuisine[randint(0, (len(company_cuisine)-1))]
-    }
-    # add obj to db here
-    store_tweet(business)
+def iterate_tweetCollection():
+    tweets_iterator = col.find()
+    return tweets_iterator
 
-print('finished creating 500 business reviews')
-
-
-# iterate through entire collection
-tweets_iterator = test.find()
-for tweet in tweets_iterator:
-    print(tweet['name'])
+    # for tweet in tweets_iterator:
+    #    print(tweet['place'])
