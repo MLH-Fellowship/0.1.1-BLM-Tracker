4bb08794c2fc62fe015a67d19e55b4861867eb6e
stellaw1 <stella01wang@gmail.com>
2020-06-04 13:27:09 -0700

    ready to integrate w mongodb iterator

--- newDbRouting.py
+++ newDbRouting.py
@@ -10,41 +10,27 @@ import threading
 import signal
 import sys
 from flask import Flask
-from flask import request
-from flask import abort
-from flask import url_for
-from flask import make_response
-from flask import Response
+from flask import render_template
 from pymongo import MongoClient
 from bson import json_util
-from threading import Thread
-
-def tail_mongo_thread():
-    client = MongoClient('mongodb://localhost:27017')
-    db = client.tweetDatabase
-    coll = db.tweetCollection
-
-    cursor = coll.find({},tailable=True,timeout=False)
-
-    # iterate through db and send to front end
 
 
 app = Flask(__name__)
-@app.route('/tweets')
-def tweets():
-    url_for('static', filename='map.html')
-    url_for('static', filename='jquery-1.7.2.min.js')
-    url_for('static', filename='jquery.eventsource.js')
-    url_for('static', filename='jquery-1.7.2.js')
-    return Response(event_stream(), headers={'Content-Type':'text/event-stream'})
 
-def runThread():
-    st = Thread( target = tail_mongo_thread )
-    st.start()
+@app.route("/")
+def fetch_tweets():
+  client = MongoClient('mongodb://localhost:27017')
+  db = client.tweetDatabase
+  coll = db.tweetCollection
+
+  cursor = coll.find({},tailable=True,timeout=False)
+
+  # iterate through db and send to front end
+
+  # example of data to pass to js file 
+  data = {'username': 'tweet', 'site': 'stackoverflow.com'}
+  return render_template('newHeatmap.html', data=data)
 
 
 if __name__ == '__main__':
-    signal.signal(signal.SIGINT, signal_handler)
-    app.before_first_request(runThread)
     app.run(debug=True, host='127.0.0.1')
-
