56b4efab077fee0e5eaefbb81b3534610a63fb36
Parthiv Chigurupati <parthivc0930@gmail.com>
2020-06-05 11:57:00 -0400

    Updated all methods

--- sentimentAnalysis/kerasTest.py
+++ sentimentAnalysis/kerasTest.py
@@ -28,6 +28,7 @@ VALIDATION_SPLIT = 0.2
 
 dataFile = "testData.csv"  # TODO: Change
 embeddingsFile = "glove.6B.100d.txt"
+modelName = 'model.h5'
 
 embeddings_index = dict()
 data = pd.read_csv((os.path.join(BASE_DIR, dataFile)), encoding='latin-1')
@@ -36,16 +37,20 @@ data = pd.read_csv((os.path.join(BASE_DIR, dataFile)), encoding='latin-1')
 def generateEmbeddingsIndex():
     global embeddings_index
     print("\nGenerating embeddings index:")
+    wordVectorCount = 0
     with open(os.path.join(BASE_DIR, embeddingsFile)) as f:
-        for line in tqdm(f, total=400000):
-            word, coefs = line.split(maxsplit=1)
-            coefs = np.fromstring(coefs, 'f', sep=' ')
-            embeddings_index[word] = coefs
-    print('\nFound %s word vectors.' % len(embeddings_index))
+        wordVectorCount += len(f.readlines())
+    with open(os.path.join(BASE_DIR, embeddingsFile)) as f:
+        for line in tqdm(f, total=wordVectorCount):
+            word, vector = line.split(maxsplit=1)
+            vector = np.fromstring(vector, 'f', sep=' ')
+            embeddings_index[word] = vector
+    print('Found {} word vectors'.format(wordVectorCount))
 
 
 def ingestData():
     global data
+    print('\nIngesting tweet dataset')
 
     # Appending top row to the bottom to add column labels to data
     topRow = pd.DataFrame(data.iloc[0])
@@ -57,6 +62,7 @@ def ingestData():
 
     # Scale down sentiment values and convert them to ints from strings
     data.replace({'Sentiment': {4: 2, 2: 1, 0: 0}}, inplace=True)
+    print("Ingested tweet dataset")
 
 
 def tokenFilter(token):
@@ -83,31 +89,38 @@ def postProcess():
 
 def tokenizeSequences(texts, labels):
     global data
-    print('Found %s texts' % len(texts))
-    # finally, vectorize the text samples into a 2D integer tensor
+    print('\nScrubbed {} texts'.format(len(texts)))
+
+    # Tokenizing tweets
     kerasTokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
     kerasTokenizer.fit_on_texts(texts)
+
+    # Converting tweets to sequences
     sequences = kerasTokenizer.texts_to_sequences(texts)
+
+    # Padding sequences to make them equal length
     data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+
     wordCount = kerasTokenizer.word_index
-    print('Found %s unique tokens.' % len(wordCount))
+    print('Found {} unique tokens'.format(len(wordCount)))
+
+    # Making labels categorical
     labels = to_categorical(np.asarray(labels))
-    # TODO: Remove
-    print('Shape of data tensor:', data.shape)
-    print('Shape of label tensor:', labels.shape)
-    ###
+
     return wordCount, labels
 
 
 def prepareKerasData(labels):
     global data
-    # split the data into a training set and a validation set
+
+    # Split data into training and training validation data
     indices = np.arange(data.shape[0])
     np.random.shuffle(indices)
     data = data[indices]
     labels = labels[indices]
     num_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
 
+    # Splitting data into inputs and outputs
     x_train = data[:-num_validation_samples]
     y_train = labels[:-num_validation_samples]
     x_val = data[-num_validation_samples:]
@@ -127,43 +140,38 @@ def generateEmbeddingsMatrix(word_index):
             embedding_matrix[i] = embedding_vector
     return num_words, embedding_matrix
 
-######################################################
+
+def main():
+    generateEmbeddingsIndex()
+    ingestData()
+    texts, labels = postProcess()
+    word_index, labels = tokenizeSequences(texts, labels)
+    x_train, y_train, x_val, y_val = prepareKerasData(labels)
+    num_words, embedding_matrix = generateEmbeddingsMatrix(word_index)
+
+    embedding_layer = Embedding(num_words, EMBEDDING_DIM, embeddings_initializer=Constant(embedding_matrix), input_length=MAX_SEQUENCE_LENGTH, trainable=False)
+
+    # train a 1D convnet with global maxpooling
+    sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+    embedded_sequences = embedding_layer(sequence_input)
+    x = Conv1D(128, 5, activation='relu')(embedded_sequences)
+    x = MaxPooling1D(5)(x)
+    x = Conv1D(128, 5, activation='relu')(x)
+    x = MaxPooling1D(5)(x)
+    x = Conv1D(128, 5, activation='relu')(x)
+    x = GlobalMaxPooling1D()(x)
+    x = Dense(128, activation='relu')(x)
+    preds = Dense(3, activation='softmax')(x)
+
+    model = Model(sequence_input, preds)
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['acc'])
+
+    print("\nTraining model: ")
+    model.fit(x_train, y_train, batch_size=128, epochs=15, validation_data=(x_val, y_val))
+
+    model.save(modelName)
+    print("\nModel file saved as {}, training complete\n".format(modelName))
 
 
-generateEmbeddingsIndex()
-print('\nProcessing text dataset')
-ingestData()
-texts, labels = postProcess()
-word_index, labels = tokenizeSequences(texts, labels)
-x_train, y_train, x_val, y_val = prepareKerasData(labels)
-num_words, embedding_matrix = generateEmbeddingsMatrix(word_index)
-
-######################################################
-
-embedding_layer = Embedding(num_words, EMBEDDING_DIM, embeddings_initializer=Constant(embedding_matrix), input_length=MAX_SEQUENCE_LENGTH, trainable=False)
-
-print('Training model.')
-
-# train a 1D convnet with global maxpooling
-sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
-embedded_sequences = embedding_layer(sequence_input)
-x = Conv1D(128, 5, activation='relu')(embedded_sequences)
-x = MaxPooling1D(5)(x)
-x = Conv1D(128, 5, activation='relu')(x)
-x = MaxPooling1D(5)(x)
-x = Conv1D(128, 5, activation='relu')(x)
-x = GlobalMaxPooling1D()(x)
-x = Dense(128, activation='relu')(x)
-preds = Dense(3, activation='softmax')(x)
-
-model = Model(sequence_input, preds)
-model.compile(loss='categorical_crossentropy',
-              optimizer='rmsprop',
-              metrics=['acc'])
-
-model.fit(x_train, y_train,
-          batch_size=128,
-          epochs=10,
-          validation_data=(x_val, y_val))
-
-model.save('model.h5')
+if __name__ == '__main__':
+    main()
