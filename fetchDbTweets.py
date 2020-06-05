fad2b3f14cfcaa6ace90d6d0c225478a3615c695
stellaw1 <stella01wang@gmail.com>
2020-06-05 00:09:10 -0700

    we have our minimum viable product heatmap

--- fetchDbTweets.py
+++ fetchDbTweets.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 def fetch_tweets():
   client = MongoClient('mongodb://localhost:27017')
   db = client.tweetDatabase
-  coll = db.testCol
+  coll = db.tweetCollection
 
   # goes through tweets and adds lat and long of tweets into 'data' so 'data' can later be passed into the html file
   docs = coll.find()
@@ -27,15 +27,12 @@ def fetch_tweets():
   for item in docs:
     # print(item['place']['bounding_box']['coordinates'][0][0][0])
     s = '{\"lat\": ' + str(item['place']['bounding_box']['coordinates'][0][0][0]) + ', \"long\": ' + str(item['place']['bounding_box']['coordinates'][0][0][1]) + '}'
-    print(s + "\n")
     data.append(s)
 
   separator = ', '
   arrayStr = separator.join(data)
-  print(arrayStr + "\n")
 
   dataStr = '{\"array\":[' + arrayStr + ']}'
-  print(dataStr)
 
   return render_template('heatmap.html', data=dataStr)    # TODO: replace "test.html" with correct html file name
 
